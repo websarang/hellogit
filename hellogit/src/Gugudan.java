19f94a3a329cd4f38fb7879e0cb0065bca9661b8
websarang <websrang@gmail.com>
2012-04-28 15:01:23 +0900

    init

---
+++ hellogit/src/Gugudan.java
@@ -0,0 +1,34 @@
+
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("gugudanfrom:");
+		Scanner sc = new Scanner(System.in);
+
+		int i=sc.nextInt();
+
+		int i ;
+		printDan();
+
+	}
+
+	public static void printDan() {
+		print();
+	}
+
+	public static void print() {
+		for (int i = 1; i < 9; i++) {
+			int j;
+			int i1 = k * j;
+			System.out.println(i1 + " x" + j + " =" + k);
+		}
+	}
+}
